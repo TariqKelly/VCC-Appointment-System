3a52fbadd533c4465085fd5b071b91f03ca71682
De'Anna-Shanae Beadle <71797648+Deezum@users.noreply.github.com>
2021-11-29 23:11:44 -0500

    Update patientreminder.py

--- patientreminder.py
+++ patientreminder.py
@@ -2,19 +2,20 @@ import smtplib
 from datetime import datetime
 
 def patientreminder():
-
- server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
- server.login("comp2140project2021@gmail.com", "softwareengineering")
- mess = input("Enter patient's email address: ")
- name = input("Enter patient's name: ")
- appoint= input("Enter the patient's appointment date: ")
- time= input("Enter the time of the patient's appointment: ")
- server.sendmail(
+  server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+  server.login("comp2140project2021@gmail.com", "softwareengineering")
+  mess = input("Enter patient's email address: ")
+  name = input("Enter patient's name: ")
+  appoint= input("Enter the patient's appointment date: ")
+  time= input("Enter the time of the patient's appointment: ")
+  body = " "+"\r\n"+"Hello " + name + "! "+"Your upcoming appointment at Vision Care Centre on " + appoint + " at " +time+ " is coming up. If there are any changes or concerns, please contact our office. Have a great day!"
+  message = 'Subject: {}\n\n{}'.format("Upcoming Visioncare Appointment", body)
+  server.sendmail(
   
-   "comp2140project2021@gmail.com", 
+  "comp2140project2021@gmail.com", 
   
-   mess, 
+  mess, 
   
-   "Hello " + name + "! " "Your upcoming appointment at Vision Care Centre on " + appoint + " at " + time + " is coming up. If there are any changes or concerns, please contact our office. Have a great day!")
- print("Email sent to " + name)
- server.quit()
+  message)
+  print("Email sent to " + name)
+  server.quit()
